6df6acde468bd999c1a606c32414aaa23ffbe424
Ricard Lambea Jané <56436468+ricardlambea@users.noreply.github.com>
2023-11-29 20:23:21 +0100

    Update script.py

--- script.py
+++ script.py
@@ -3,6 +3,11 @@
 import time
 
 class Question():
+    """
+    Class that defines a question, keeps the user answer, keeps the
+    counter updated, and prints out some messages while the game is 
+    running.
+    """
 
     def __init__(self, question, ans_type, correct_answer, level, counter):
 
@@ -18,6 +23,7 @@ class Question():
 
         # Get answer from user input
         self.user_answer = self.get_answer()
+        
         # Give the right format to answer
         self.process_answer()
 
@@ -34,12 +40,15 @@ class Question():
         self.print_counter()
 
     def make_question(self):
+        """Print the question on stdout"""
         print(self.question)
 
     def get_answer(self):
+        """Returns the user's answer"""
         return input()
     
     def process_answer(self):
+        """Transform the user's answer into the right format"""
         if self.ans_type == "int":
             self.user_answer = int(self.user_answer)
         elif self.ans_type == "str":
@@ -50,12 +59,17 @@ class Question():
             raise ValueError("This answer type is not implemented yet.")
 
     def is_correct(self, level):
+        """Check if the answer is correct, and return the corresponding value"""
         if self.user_answer == self.correct_answer:
             return self.q_level(level)
         else:
             return 0
         
     def print_won_points(self, points):
+        """
+        Print on stdout a customized message depending on whether the answer 
+        was correct or not
+        """ 
         if points > 0:
             print("Yay! Correct answer!")
             print(f"You get {points} points!")
@@ -66,6 +80,7 @@ class Question():
             print()
 
     def q_level(self, level):
+        """Return a value depending on the question's difficulty"""
         if level == 'easy':
             return 5
         elif level == 'mid':
@@ -75,25 +90,37 @@ class Question():
         
     @property
     def points(self):
+        """Return the points won in the current question"""
         return self._points
     
     def print_counter(self):
+        """Print the counter value on stdout"""
         print(f"Right now your counter has {self.get_counter} points.") 
         print()
 
     @property
     def get_counter(self):
+        """Return the value of the current counter"""
         return self._counter
 
     def sum_to_counter(self):
+        """
+        Sum the points won in the current question to the points that were 
+        already on the counter
+        """
         self._counter = self._counter + self.points
 
 
-
 if __name__=='__main__':
-        
-    # Suported types of question: 'str', 'int', 'float', "" (for free asnwer questions)
 
+    print(
+        "This is The HaGE (Hardest Game Ever). A trivial-like mini-game in which \n" +
+        "you have to answer some questions, and for each question, and difficulty, \n" +
+        "you will get different points. Try to gain as many points as you can. \n" +
+        "Good luck!!!\n\n"
+    )
+    # Suported types of questions: 'str', 'int', 'float'
+    # You start with a 0 on your counter 
     q1 = Question("Which is the root square of 452?", "float", "21.26", "hard", 0)
     q2 = Question("Write the number 4 in binary", "str", "100", "mid", q1.get_counter)
     q3 = Question("How many planets are there in the solar system?", "int", "8", "easy", q2.get_counter)
